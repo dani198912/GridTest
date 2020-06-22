30f43661df8fe76f72dc07d0046d7b0033c61393
dani198912 <Feborcesim9>
2020-06-22 15:51:30 +0300

    Making the gantt interactive

--- src/javascript.js
+++ src/javascript.js
@@ -1,16 +1,17 @@
 let hoursContainer = document.getElementsByClassName("hours-container");
-
+const ITERS = 96;
 
 
 for(let i = 0; i < hoursContainer.length; i++) {
     console.log("Inside hours-container");
-    for (let s = 0; s < 24; s++) {
+    for (let s = 0; s < ITERS; s++) {
         const span = document.createElement("span");
+        span.className = `D-${Math.floor(s/24)}-H-${s%24}`;
         span.style.gridColumn = `${s+1}`;
         hoursContainer[i].appendChild(span);
         
     }
-    for (let k = 0; k < 24; k++) {
+    for (let k = 0; k < ITERS; k++) {
         let hours = document.createElement("div");
         hours.className = "hour";
         hours.innerText = `${k < 10 ? "0" + k : k%24}:00`;
@@ -18,7 +19,7 @@ for(let i = 0; i < hoursContainer.length; i++) {
         console.log(`At ${k}`);
         hoursContainer[i].appendChild(hours);        
     }
-    for (let t = 2; t < 6; t++) {
+    for (let t = 2; t < 8; t++) {
         let task = document.createElement("div");
         task.className = "task";
         task.innerText =`The task ${t} is here`;
